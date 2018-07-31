1e9ad48f990ed2b97b614085e0e5bc6208b8c1d7
Guy <guy@tssgroup.com.au>
2018-07-31 13:03:06 +1000

    fix iphone header

--- _site/assets/js/plugins.js
+++ _site/assets/js/plugins.js
@@ -85,3 +85,13 @@ $('#videoModal').children().filter("video").each(function(){
 });
 $('#videoModal').empty();
 })*/
+  
+// Add slideDown animation to Bootstrap dropdown when expanding.
+  $('.dropdown').on('show.bs.dropdown', function() {
+    $(this).find('.dropdown-menu').first().stop(true, true).slideDown();
+  });
+
+  // Add slideUp animation to Bootstrap dropdown when collapsing.
+  $('.dropdown').on('hide.bs.dropdown', function() {
+    $(this).find('.dropdown-menu').first().stop(true, true).slideUp();
+  });
